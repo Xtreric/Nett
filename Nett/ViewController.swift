35708dd7a47231c2b7ca7ce9c6ce69bd54020186
Eric Huang <erichuang@rdwebimdemac-pro.local>
2016-07-15 12:12:55 +0800

    Initial Commit

---
+++ Nett/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Nett
+//
+//  Created by Eric Huang on 2016/7/15.
+//  Copyright © 2016年 WeBIM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
